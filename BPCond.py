2f5b08031fd8c7ecf8b137e0646d6cda832069ef
0xgalz <41758817+0xgalz@users.noreply.github.com>
2018-11-30 10:38:11 +0200

    Updated full vtables creation
    Updated full vtables creation

--- BPCond.py
+++ BPCond.py
@@ -6,50 +6,60 @@
 #    * offset -> The offset of the relevant function from the vtable base pointer
 #import idaapi
 
-virtual_call_addr = str(<<<start_addr>>>)  #idc.NextHead(idc.here())
+try:
+    virtual_call_addr = str(<<<start_addr>>>)  #idc.NextHead(idc.here())
 
-# Get the adresses of the  vtable and the virtual function from the relevant register:
-p_vtable_addr = idc.GetRegValue("<<<register_vtable>>>")
-pv_func_addr = idc.GetRegValue("<<<register_vtable>>>") + <<<offset>>>
-v_func_addr = get_wide_dword(pv_func_addr)
+    # Get the adresses of the  vtable and the virtual function from the relevant register:
+    p_vtable_addr = idc.GetRegValue("<<<register_vtable>>>")
+    pv_func_addr = idc.GetRegValue("<<<register_vtable>>>") + <<<offset>>>
+    v_func_addr = get_wide_dword(pv_func_addr)
 
-# calculate the offset of the virtual function from the beginning of its section, determine its name
-# in case the function name was changed by the user the name will stay the same
-v_func_name = GetFunctionName(v_func_addr)
-if v_func_name[:4] == "sub_":
-    v_func_name =  "vfunc_" + str(v_func_name)[4:] # str(hex(v_func_addr- SegStart(v_func_addr))) the name will be the offset from the beginning of the segment
-    idaapi.set_name(v_func_addr, v_func_name, idaapi.SN_FORCE)
+    # calculate the offset of the virtual function from the beginning of its section, determine its name
+    # in case the function name was changed by the user the name will stay the same
+    v_func_name = GetFunctionName(v_func_addr)
+    if v_func_name[:4] == "sub_":
+        v_func_name =  "vfunc_" + str(v_func_name)[4:] # str(hex(v_func_addr- SegStart(v_func_addr))) the name will be the offset from the beginning of the segment
+        idaapi.set_name(v_func_addr, v_func_name, idaapi.SN_FORCE)
 
-print v_func_name
+    print v_func_name
 
-# calculate the offset of the vtable from the beginning of its section, determine its name
-calc_struct_name =  int(p_vtable_addr) - SegStart(p_vtable_addr)
-vtable_addr_name = "vtable_" + str(calc_struct_name) #  str(p_vtable_addr) +
+    # calculate the offset of the vtable from the beginning of its section, determine its name
+    calc_struct_name =  int(p_vtable_addr) - SegStart(p_vtable_addr)
+    vtable_addr_name = "vtable_" + str(calc_struct_name) #  str(p_vtable_addr) +
 
-#change vtable address name
-idaapi.set_name(p_vtable_addr, vtable_addr_name, idaapi.SN_FORCE)
+    #change vtable address name
+    idaapi.set_name(p_vtable_addr, vtable_addr_name, idaapi.SN_FORCE)
 
-# create the vtable struct
-struct_name = vtable_addr_name + "_struct"
-struct_id = add_struc(-1, struct_name, 0)
-print struct_id, struct_name
-if struct_id != idc.BADADDR: # checks if the struct creation succeeded
-    # add structure members and change the offset structure in the assembly
-    idc.add_struc_member(struct_id, v_func_name, <<<offset>>> , FF_DWRD, -1, 4)
-    #FIXIT: idc.SetMemberComment(struct_id, """ + offset + """ , "Was called form address:" + str(hex(idc.GetRegValue("eip"))) , 1)
-    print "%%%%structure_id%%%%", struct_id #idc.GetRegValue("eip")
-    idc.OpStroff(idautils.DecodeInstruction(int(idc.GetRegValue("eip"))), 1, struct_id)
-else:
-    struct_id = idc.GetStrucIdByName(struct_name)
-    if struct_id != idc.BADADDR: #checks if the struct exists, in this case the function offset will be added to the struct
-        idc.add_struc_member(struct_id, v_func_name, <<<offset>>> , FF_DWRD, -1, 4)
+    # create the vtable struct
+    struct_name = vtable_addr_name + "_struct"
+    struct_id = add_struc(-1, struct_name, 0)
+    print struct_id, struct_name
+    if struct_id != idc.BADADDR: # checks if the struct creation succeeded
+        # add all the functions of the vtable to the struct
+        vtable_func_offset = 0
+        vtable_func_value = get_wide_dword(p_vtable_addr)
+        while vtable_func_value != 0:
+            v_func_name = GetFunctionName(vtable_func_value)
+            if v_func_name[:4] == "sub_":
+                v_func_name =  "vfunc_" + str(v_func_name)[4:] # str(hex(v_func_addr- SegStart(v_func_addr))) the name will be the offset from the beginning of the segment
+                idaapi.set_name(vtable_func_offset, v_func_name, idaapi.SN_FORCE)
+            idc.add_struc_member(struct_id, v_func_name, vtable_func_offset , FF_DWRD, -1, 4)
+            vtable_func_offset += 4
+            vtable_func_value = get_wide_dword(p_vtable_addr + vtable_func_offset)
+
+        #FIXIT: idc.SetMemberComment(struct_id, """ + offset + """ , "Was called form address:" + str(hex(idc.GetRegValue("eip"))) , 1)
         idc.OpStroff(idautils.DecodeInstruction(int(idc.GetRegValue("eip"))), 1, struct_id)
-        print "WWEEEEEEE ARRREE TTTHHHEEE CHEMPPPIOONNNSSSSS MY EDX+4!!!!"
     else:
-        print "Failed to create struct: " +  struct_name
-    #add comments to the assembly
-    #last_text = idc.get_cmt(virtual_call_addr, 1)
-    #if last_text == None:
-        #last_text = ""
-    #idc.set_cmt(virtual_call_addr, last_text + "vtable struct is: " +idaapi.get_struc_name(struct_id) + ", function: " + v_func_name, 1)
+        struct_id = idc.GetStrucIdByName(struct_name)
+        if struct_id != idc.BADADDR: #checks if the struct exists, in this case the function offset will be added to the struct
+            idc.OpStroff(idautils.DecodeInstruction(int(idc.GetRegValue("eip"))), 1, struct_id)
+        else:
+            print "Failed to create struct: " +  struct_name
+        #add comments to the assembly
+        #last_text = idc.get_cmt(virtual_call_addr, 1)
+        #if last_text == None:
+            #last_text = ""
+        #idc.set_cmt(virtual_call_addr, last_text + "vtable struct is: " +idaapi.get_struc_name(struct_id) + ", function: " + v_func_name, 1)
+except:
+    print "General Error Running the script :O"
 1 == 1
